98558c28ac2db8b5d2f7b1d6a99fedd3cdf7f34c
BekirK-C <b.b.kamac@gmail.com>
2022-11-02 15:39:10 +0300

    Active button in brands jquery implementation

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -9,7 +9,7 @@ import { BrandService } from 'src/app/services/brand.service';
 })
 export class BrandComponent implements OnInit {
   brands: Brand[] = [];
-  //currentBrand: Brand;
+  currentBrand: Brand;
   dataLoaded = false;
   constructor(private brandService: BrandService) {}
 
@@ -25,7 +25,14 @@ export class BrandComponent implements OnInit {
   }
 
   setCurrentBrand(brand: Brand) {
-    //this.currentBrand = brand;
-    console.log(brand.name);
+    this.currentBrand = brand;
+  }
+
+  getCurrentBrandClass(brand: Brand) {
+    if (brand == this.currentBrand) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
   }
 }
