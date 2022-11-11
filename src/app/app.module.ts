7c384e5c26e4bfbf2c321b055002090feb2001f4
BekirK-C <b.b.kamac@gmail.com>
2022-11-11 13:43:38 +0300

    pipes and garage services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,12 @@ import { NaviComponent } from './components/navi/navi.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { CarDetailsComponent } from './components/car-details/car-details.component';
 import { CarImageComponent } from './components/car-image/car-image.component';
+import { FilterColorPipe } from './pipes/filter-color.pipe';
+import { FilterCarPipe } from './pipes/filter-car.pipe';
+import { FilterBrandPipe } from './pipes/filter-brand.pipe';
+import { GarageComponent } from './components/garage/garage.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 @NgModule({
   declarations: [
@@ -24,8 +31,22 @@ import { CarImageComponent } from './components/car-image/car-image.component';
     CustomerComponent,
     CarDetailsComponent,
     CarImageComponent,
+    FilterColorPipe,
+    FilterCarPipe,
+    FilterBrandPipe,
+    GarageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/services/garage.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Car } from '../models/car';
+import { GarageItem } from '../models/garageItem';
+import { GarageItems } from '../models/garageItems';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GarageService {
+
+  constructor() { }
+
+  addToGarage(car: Car) {
+    let item = GarageItems.find((c) => c.car.id === car.id);
+    if (item) {
+      item.quantity++;
+    } else {
+      let garageItem = new GarageItem();
+      garageItem.car = car;
+      garageItem.quantity = 1;
+      GarageItems.push(garageItem);
+    }
+  }
+  removeFromGarage(car: Car) {
+    let item: GarageItem = GarageItems.find(c => c.car.id === car.id);
+    GarageItems.splice(GarageItems.indexOf(item), 1);
+  }
+
+  list(): GarageItem[] {
+    return GarageItems;
+  }
+}
